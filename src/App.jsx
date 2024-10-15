6be58f46fd61298f0e2e62848809ef2762987b17
Anup Shrestha <anupshresth9@gmail.com>
2024-10-16 00:22:07 +0545

    project Color switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import Card from "./components/card";
+import { useState } from "react";
 
 const App = () => {
+  const [color, setColor] = useState("olive");
+
   return (
     <>
-      <Card title="Anup" description="God like character" />
-      <Card title="Harry" description="God like character" />
-      <Card title="Harry" />
-      <Card description="God like character" />
-      <Card />
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg capitalize bg-red-500"
+              onClick={() => setColor("red")}
+            >
+              Red
+            </button>
+            <button
+              className="outline-none px-4 py-1 rounded-full text-black shadow-lg capitalize bg-green-500"
+              onClick={() => setColor("green")}
+            >
+              Green
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
